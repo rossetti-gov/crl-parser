d2ded07bbef7df503679102faf79e2727b84e039
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-01 18:00:53 -0500

    Write CRL summary data to file

--- jobs/parser.rb
+++ jobs/parser.rb
@@ -1,4 +1,5 @@
 require "openssl"
+require "json"
 require "pry"
 
 CRL_URL = "http://crl.disa.mil/crl/DODIDCA_42.crl" # this is the only CRL URL I know about at the moment.
@@ -12,23 +13,38 @@ puts "DOWNLOADING CERTIFICATE REVOCATION LIST TO #{crl_filepath}..."
 puts "PARSING CERTIFICATE REVOCATION LIST..."
 
 crl = OpenSSL::X509::CRL::new(File.read(crl_filepath))
-revocations = crl.revoked.map { |revocation|
-  {
-    serial_number: revocation.serial.to_s,
-    revoked_at: revocation.time.to_s,
-    extensions: revocation.extensions.map{ |ext| ext.to_h }
-  }
-} #.sort_by{ |r| r[:_____]}
+revocations = crl.revoked
+revocations = revocations.sort_by{ |revocation| revocation.time }
 
 metadata = {
   issuer: crl.issuer.to_s,
   version: crl.version.to_s,
   last_update: crl.last_update.to_s,
   next_update: crl.next_update.to_s,
-  revocations_count: revocations.count,
-  earlist_revocation_at: revocations.first[:revoked_at],
-  latest_revocation_at: revocations.last[:revoked_at],
-  revocations: revocations
+  revocations_count: revocations.count, #> 306926
+  earlist_revocation_at: revocations.first.time,
+  latest_revocation_at: revocations.last.time
 }
 
 pp metadata
+
+metadata_filepath = File.join(crl_dir, "metadata.json")
+File.open(metadata_filepath, "w") do |f|
+  f.write(JSON.pretty_generate(metadata))
+end
+
+#puts "PARSING REVOKED CERTIFICATES..."
+#
+#r = []
+#revocations.each do |revocation|
+#  r << {
+#    serial_number: revocation.serial.to_s,
+#    revoked_at: revocation.time.to_s,
+#    extensions: revocation.extensions.map{ |ext| ext.to_h }
+#  }
+#end
+#
+#revocations_filepath = File.join(crl_dir, "revocations.json")
+#File.open(revocations_filepath ,"w") do |f|
+#  f.write(r.to_json)
+#end
