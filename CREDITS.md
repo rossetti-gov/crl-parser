df27dd033511bce21678bde916c90cb90b424420
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-01 15:55:27 -0500

    Prepare to parse CRL from file

---
+++ CREDITS.md
@@ -0,0 +1,7 @@
+# Credits, Notes, and Reference
+
+  + [OpenSSL Docs](https://www.openssl.org/docs/man1.1.0/apps/crl.html)
+  + [Ruby OpenSSL Source](https://github.com/ruby/openssl)
+  + [Ruby `OpenSSL::X509::CRL` Docs](https://ruby-doc.org/stdlib-2.4.2/libdoc/openssl/rdoc/OpenSSL/X509/CRL.html)
+  + [Parsing a CRL with OpenSSL](https://langui.sh/2010/01/10/parsing-a-crl-with-openssl/)
+  + [Reading CRLs in Windows with Ruby](http://seanbachelder.me/2016/06/17/reading-crls-in-windows-with-ruby.html)
--- jobs/parser.rb
+++ jobs/parser.rb
@@ -1,1 +1,12 @@
-puts "PARSING A CERTIFICATE REVOCATION LIST..."
+
+#require "openssl"
+
+CRL_URL = "http://crl.disa.mil/crl/DODIDCA_42.crl" # this is the only CRL URL I know about at the moment.
+
+puts "FETCHING CERTIFICATE REVOCATION LIST FROM #{CRL_URL}"
+
+crl_filepath = "./data/20180301-1544/DODIDCA_42.crl" # NOTE: file when opened with a text editor appears to be binary, which would indicate the DER format (not the PEM format)
+
+puts "DOWNLOADED CERTIFICATE REVOCATION LIST TO #{crl_filepath}"
+
+#parsed_crl = R509::CRL::SignedList.new(crl)
