b5a1690d310d1eb658342c6f197fca2661f73848
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-01 16:59:26 -0500

    Parse CRL and one of the revoked certs

--- CREDITS.md
+++ CREDITS.md
@@ -3,6 +3,7 @@
   + [OpenSSL Docs](https://www.openssl.org/docs/man1.1.0/apps/crl.html)
   + [Ruby OpenSSL Source](https://github.com/ruby/openssl)
   + [Ruby `OpenSSL::X509::CRL` Docs](https://ruby-doc.org/stdlib-2.4.2/libdoc/openssl/rdoc/OpenSSL/X509/CRL.html)
+  + [Ruby `OpenSSL::X509::Revoked` Docs](http://ruby-doc.org/stdlib-2.5.0/libdoc/openssl/rdoc/OpenSSL/X509/Revoked.html)
   + [Parsing a CRL with OpenSSL](https://langui.sh/2010/01/10/parsing-a-crl-with-openssl/)
   + [Reading CRLs in Windows with Ruby](http://seanbachelder.me/2016/06/17/reading-crls-in-windows-with-ruby.html)
 
--- jobs/parser.rb
+++ jobs/parser.rb
@@ -1,12 +1,38 @@
-
-#require "openssl"
+require "openssl"
+require "pry"
 
 CRL_URL = "http://crl.disa.mil/crl/DODIDCA_42.crl" # this is the only CRL URL I know about at the moment.
 
-puts "FETCHING CERTIFICATE REVOCATION LIST FROM #{CRL_URL}"
+puts "FETCHING CERTIFICATE REVOCATION LIST FROM #{CRL_URL}..."
 
 crl_filepath = "./data/20180301-1544/DODIDCA_42.crl" # NOTE: file when opened with a text editor appears to be binary, which would indicate the DER format (not the PEM format)
 
-puts "DOWNLOADED CERTIFICATE REVOCATION LIST TO #{crl_filepath}"
+puts "DOWNLOADED CERTIFICATE REVOCATION LIST TO #{crl_filepath}..."
 
-#parsed_crl = R509::CRL::SignedList.new(crl)
+puts "PARSING CRL FILE..."
+
+crl = OpenSSL::X509::CRL::new(File.read(crl_filepath))
+
+puts "... ISSUER: #{crl.issuer.to_s}"
+puts "... VERSION: #{crl.version.to_s}"
+puts "... LAST UPDATE: #{crl.last_update.to_s}"
+puts "... NEXT UPDATE: #{crl.next_update.to_s}"
+revoked_certs = crl.revoked # memoize for performance
+puts "... REVOKED CERTIFICATE COUNT: #{revoked_certs.count}"
+
+#puts "... LOOPING THROUGH REVOKED CERTIFICATES ..."
+
+#crl.revoked.each do |revoked_cert|
+#
+#end
+
+puts "INVESTIGATING FIRST CERTIFICATE..."
+
+r = revoked_certs.first
+
+puts "... SERIAL: #{r.serial.to_s}"
+puts "... TIME: #{r.time.to_s}"
+puts "... EXTENSIONS (#{r.extensions.count}):"
+r.extensions.each do |ext|
+  puts "   ... #{ext.to_h}"
+end
