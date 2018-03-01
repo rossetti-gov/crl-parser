580f24a3d6416c1e676c0f06ee0a5a95078a1574
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-01 17:40:41 -0500

    Initial attempt is taking a long time, need to improve performance

--- jobs/parser.rb
+++ jobs/parser.rb
@@ -2,37 +2,33 @@ require "openssl"
 require "pry"
 
 CRL_URL = "http://crl.disa.mil/crl/DODIDCA_42.crl" # this is the only CRL URL I know about at the moment.
-
 puts "FETCHING CERTIFICATE REVOCATION LIST FROM #{CRL_URL}..."
 
-crl_filepath = "./data/20180301-1544/DODIDCA_42.crl" # NOTE: file when opened with a text editor appears to be binary, which would indicate the DER format (not the PEM format)
+downloaded_at = "20180301-1544" #TODO: vary over time
+crl_dir = "./data/#{downloaded_at}"
+crl_filepath = File.join(crl_dir, "DODIDCA_42.crl")
+puts "DOWNLOADING CERTIFICATE REVOCATION LIST TO #{crl_filepath}..."
 
-puts "DOWNLOADED CERTIFICATE REVOCATION LIST TO #{crl_filepath}..."
-
-puts "PARSING CRL FILE..."
+puts "PARSING CERTIFICATE REVOCATION LIST..."
 
 crl = OpenSSL::X509::CRL::new(File.read(crl_filepath))
+revocations = crl.revoked.map { |revocation|
+  {
+    serial_number: revocation.serial.to_s,
+    revoked_at: revocation.time.to_s,
+    extensions: revocation.extensions.map{ |ext| ext.to_h }
+  }
+} #.sort_by{ |r| r[:_____]}
 
-puts "... ISSUER: #{crl.issuer.to_s}"
-puts "... VERSION: #{crl.version.to_s}"
-puts "... LAST UPDATE: #{crl.last_update.to_s}"
-puts "... NEXT UPDATE: #{crl.next_update.to_s}"
-revoked_certs = crl.revoked # memoize for performance
-puts "... REVOKED CERTIFICATE COUNT: #{revoked_certs.count}"
+metadata = {
+  issuer: crl.issuer.to_s,
+  version: crl.version.to_s,
+  last_update: crl.last_update.to_s,
+  next_update: crl.next_update.to_s,
+  revocations_count: revocations.count,
+  earlist_revocation_at: revocations.first[:revoked_at],
+  latest_revocation_at: revocations.last[:revoked_at],
+  revocations: revocations
+}
 
-#puts "... LOOPING THROUGH REVOKED CERTIFICATES ..."
-
-#crl.revoked.each do |revoked_cert|
-#
-#end
-
-puts "INVESTIGATING FIRST CERTIFICATE..."
-
-r = revoked_certs.first
-
-puts "... SERIAL: #{r.serial.to_s}"
-puts "... TIME: #{r.time.to_s}"
-puts "... EXTENSIONS (#{r.extensions.count}):"
-r.extensions.each do |ext|
-  puts "   ... #{ext.to_h}"
-end
+pp metadata
