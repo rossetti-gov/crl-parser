ea6ae5d3dc5a1652b1b2978effe3b4d9562e83e7
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-05 14:27:26 -0500

    Parse additional CRLs and store metadata

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "openssl"
 gem "pry"
+gem "activesupport"
--- jobs/parser.rb
+++ jobs/parser.rb
@@ -1,72 +1,112 @@
 require "openssl"
+require "open-uri"
 require "json"
 require "csv"
 require "pry"
+require "active_support/core_ext/object/try"
 
-CRL_URL = "http://crl.disa.mil/crl/DODIDCA_42.crl" # this is the only CRL URL I know about at the moment.
-puts "FETCHING CERTIFICATE REVOCATION LIST FROM #{CRL_URL}..."
+class Parser
+  attr_accessor :crl_url
 
-downloaded_at = "20180301-1544" #TODO: vary over time
-crl_dir = "./data/#{downloaded_at}"
-crl_filepath = File.join(crl_dir, "DODIDCA_42.crl")
-puts "DOWNLOADING CERTIFICATE REVOCATION LIST TO #{crl_filepath}..."
+  def initialize(crl_url)
+    @crl_url = crl_url
+  end
 
-puts "PARSING CERTIFICATE REVOCATION LIST..."
+  def perform
+    FileUtils.mkdir_p(data_dir)
+    puts "PARSING #{crl_url} ..."
+    pp metadata
+    write_metadata_to_json
+    puts "-------------------------------"
+  end
 
-crl = OpenSSL::X509::CRL::new(File.read(crl_filepath))
-revocations = crl.revoked
-revocations = revocations.sort_by{ |revocation| revocation.time }
+  def crl
+    @crl ||= download_crl
+  end
 
-metadata = {
-  issuer: crl.issuer.to_s,
-  version: crl.version.to_s,
-  last_update: crl.last_update.to_s,
-  next_update: crl.next_update.to_s,
-  revocations_count: revocations.count, #> 306926
-  earliest_revocation_at: revocations.first.time,
-  latest_revocation_at: revocations.last.time
-}
+  def download_crl
+    IO.copy_stream(open(crl_url), crl_filepath) unless File.exist?(crl_filepath)
+    return OpenSSL::X509::CRL::new(File.read(crl_filepath))
+  end
 
-pp metadata
+  def revocations
+    @revocations ||= crl.revoked.sort_by{ |revocation| revocation.try(:time) } # use .try because crl.revoked may be empty
+  end
 
-metadata_filepath = File.join(crl_dir, "metadata.json")
-File.open(metadata_filepath, "w") do |f|
-  f.write(JSON.pretty_generate(metadata))
+  def metadata
+    @metadata ||= {
+      issuer: crl.issuer.to_s,
+      version: crl.version.to_s,
+      last_update: crl.last_update.to_s,
+      next_update: crl.next_update.to_s,
+      revocations_count: revocations.count, #> 306926
+      earliest_revocation_at: revocations.first.try(:time),
+      latest_revocation_at: revocations.last.try(:time)
+    }
+  end
+
+  def crl_filename
+    crl_url.split("/").last
+  end
+
+  def crl_name
+    crl_filename.gsub(".crl","")
+  end
+
+  def data_dir
+    "./data/#{crl_name}/#{Date.today.to_s}"
+  end
+
+  def crl_filepath
+    File.join(data_dir, crl_filename)
+  end
+
+  def metadata_filepath
+    File.join(data_dir, "metadata.json")
+  end
+
+  def write_metadata_to_json
+    File.open(metadata_filepath, "w") do |f|
+      f.write(JSON.pretty_generate(metadata))
+    end
+  end
+
+  #def write_revocations_to_json
+  #  revs = [] # maybe faster than mapping 300K items in place...
+  #  revocations.each do |revocation|
+  #    revs << {
+  #      serial_number: revocation.serial.to_s,
+  #      revoked_at: revocation.time.to_s,
+  #      extensions: revocation.extensions.map{ |ext| ext.to_h }
+  #    }
+  #  end
+  #
+  #  revocations_filepath = File.join(crl_dir, "revocations.json")
+  #  File.open(revocations_filepath ,"w") do |f|
+  #    f.write(JSON.pretty_generate(revs)) # is there a way to write incrementally?
+  #  end
+  #end
+
+  #def write_revocations_to_csv
+  #  revocations_filepath = File.join(crl_dir, "revocations.csv")
+  #  headers = ["serial_number", "revoked_at", "extensions"]
+  #
+  #  CSV.open(revocations_filepath, "w", :write_headers=> true, :headers => headers) do |csv|
+  #    revocations.each do |revocation|
+  #      csv << [
+  #        revocation.serial.to_s,
+  #        revocation.time.to_s,
+  #        revocation.extensions.map{|ext| ext.to_s }.join(" | ") # pipe-delimited string like "CRLReason = Cessation Of Operation | invalidityDate = ..20160610050000Z"
+  #      ]
+  #    end
+  #  end
+  #end
 end
 
-puts "PARSING REVOKED CERTIFICATES..."
+KNOWN_CRL_URLS = [40, 41, 42, 43, 44, 49, 50, 51, 52].map{ |i|
+  "http://crl.disa.mil/crl/DODIDCA_#{i}.crl"
+}
 
-#
-# REVOCATIONS.JSON
-#
-#
-#revs = [] # maybe faster than mapping 300K items in place...
-#revocations.each do |revocation|
-#  revs << {
-#    serial_number: revocation.serial.to_s,
-#    revoked_at: revocation.time.to_s,
-#    extensions: revocation.extensions.map{ |ext| ext.to_h }
-#  }
-#end
-#
-#revocations_filepath = File.join(crl_dir, "revocations.json")
-#File.open(revocations_filepath ,"w") do |f|
-#  f.write(JSON.pretty_generate(revs)) # is there a way to write incrementally?
-#end
-
-#
-# REVOCATIONS.CSV
-#
-
-revocations_filepath = File.join(crl_dir, "revocations.csv")
-headers = ["serial_number", "revoked_at", "extensions"]
-
-CSV.open(revocations_filepath, "w", :write_headers=> true, :headers => headers) do |csv|
-  revocations.each do |revocation|
-    csv << [
-      revocation.serial.to_s,
-      revocation.time.to_s,
-      revocation.extensions.map{|ext| ext.to_s }.join(" | ") # pipe-delimited string like "CRLReason = Cessation Of Operation | invalidityDate = ..20160610050000Z"
-    ]
-  end
+KNOWN_CRL_URLS.each do |crl_url|
+  Parser.new(crl_url).perform
 end
